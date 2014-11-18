24de078475c55d78583b63206dcb004113852499
ericreber <eric.reber@rscfirm.com>
2014-11-18 13:19:36 +0100

    Draggable view and  gesture recognizer

---
+++ GGDraggableView.swift
@@ -0,0 +1,80 @@
+//
+//  GGDraggableView.swift
+//  SwiftSwipeCards
+//
+//  Created by Eric Reber on 18.11.14.
+//  Copyright (c) 2014 Eric Reber. All rights reserved.
+//
+
+import UIKit
+
+class GGDraggableView: UIView {
+
+    var panGestureRecognizer : UIPanGestureRecognizer!
+    var originalPoint:CGPoint!
+    
+    override init(){
+        super.init()
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame : frame)
+        self.backgroundColor = UIColor.greenColor()
+        
+        
+        self.panGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("dragged:"))
+        self.addGestureRecognizer(panGestureRecognizer)
+        
+        self.loadImageAndStyle()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    func loadImageAndStyle(){
+        self.layer.cornerRadius = 8
+        self.layer.shadowOffset = CGSizeMake(7.00, 7.00)
+        self.layer.shadowRadius = 5
+        self.layer.shadowOpacity = 0.5
+    }
+    
+    func dragged(gestureRecognizer: UIPanGestureRecognizer) {
+  
+        let xDistance:CGFloat = gestureRecognizer.translationInView(self).x
+        let yDistance:CGFloat = gestureRecognizer.translationInView(self).y
+        
+        switch(gestureRecognizer.state){
+        case UIGestureRecognizerState.Began:
+            self.originalPoint = self.center
+            
+        case UIGestureRecognizerState.Changed:
+            let rotationStrength:CGFloat = min((xDistance/320),1)
+            let rotationAngel:CGFloat = (2.00*CGFloat(M_PI)*CGFloat(rotationStrength) / 16.00)
+            let scaleStrength:CGFloat = 1.00 - CGFloat(fabsf(Float(rotationStrength))) / 4.00
+            let scale:CGFloat = max(scaleStrength, 0.93);
+            
+            self.center = CGPointMake(self.originalPoint.x + xDistance, self.originalPoint.y + yDistance)
+            let transform:CGAffineTransform = CGAffineTransformMakeRotation(rotationAngel)
+            let scaleTransform:CGAffineTransform = CGAffineTransformScale(transform, scale, scale)
+            self.transform = scaleTransform
+            
+        case UIGestureRecognizerState.Ended:
+            self.resetViewPositionAndTransformations()
+        
+            
+        default:
+            println("error default statement")
+            
+        }
+    }
+    
+    func resetViewPositionAndTransformations(){
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            self.center = self.originalPoint
+            self.transform = CGAffineTransformMakeRotation(0)
+        })
+    }
+
+}
---
+++ GGView.swift
@@ -0,0 +1,38 @@
+//
+//  GGView.swift
+//  SwiftSwipeCards
+//
+//  Created by Eric Reber on 18.11.14.
+//  Copyright (c) 2014 Eric Reber. All rights reserved.
+//
+
+import UIKit
+
+class GGView: UIView {
+
+    var draggableView:GGDraggableView!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        println("InitGGView")
+        self.backgroundColor = UIColor.whiteColor()
+        self.loadDraggableCustomView()
+    }
+    
+    func loadDraggableCustomView(){
+        self.draggableView = GGDraggableView()
+        self.draggableView.backgroundColor = UIColor.blueColor()
+        self.draggableView.frame = CGRectMake(60, 60, 200, 260)
+        self.addSubview(self.draggableView)
+        
+    }
+    
+    func layoutSubViews()
+    {
+        super.layoutSubviews()
+        self.draggableView.frame = CGRectMake(60, 60, 200, 260)
+    }
+    
+    
+}
